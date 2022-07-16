c1c19025b6b8178acae06de60f88bc9fcaabb3ce
nkalinich <nataliekalinich@gmail.com>
2022-07-16 14:57:12 +0300

    heroes

---
+++ script.js
@@ -0,0 +1,54 @@
+// В Array.prototype добавить свойство с названием heroesRender в значении которого находится функция.
+// Даная функция может вызываться в контексте любого массива 
+// и в качестве входящего аргумента принимает название папки ('dc', 'marvel'), с которой будут подтягиваться иконки супер-героев этого массива. 
+// Пример:
+// dcHeroes.heroesRender('dc');
+// marvelHeroes.heroesRender('marvel');
+// Функция возвращает таблицу, в которой выводится информация по каждому из вложенных объектов. 
+
+// debugger;
+
+let marvelHeroes = [
+	{name: "Thor"},
+	{name: "Spider Man"},
+];
+
+let dcHeroes = [
+	{name: "Superman"},
+	{name: "Batman"},
+	{name: "Deadpool"},
+];
+
+
+Array.prototype.heroesRender = function (folder) {
+
+	let trs = this.map(
+		function(heroes) {
+			return `
+				<tr>
+					<td>${heroes.name}</td>
+					<td>
+						<img src="images/${folder}/${heroes.name.toLowerCase().replaceAll(" ", "")}.svg">
+					</td>
+				</tr>
+			`
+		}
+	)
+
+	document.write(` 
+		<table>
+			<thead>
+				<tr>
+					<th>Name</th>
+					<th>Icon</th>
+				</tr>
+			</thead>
+			<tbody>
+				${trs}
+			</tbody>
+		</table>
+	`);
+};
+
+marvelHeroes.heroesRender("marvel");
+dcHeroes.heroesRender("dc");
